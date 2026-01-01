9c19045068344bc226f576fe21b6407c13a13dc1
Ravelly Carvalho Zanatta <ravelly.z@gmail.com>
2026-01-01 17:13:28 -0300

    Adicionei o router.py que possui a função responsável em definir a fluxo do LangGraph

---
+++ app/agents/router.py
@@ -0,0 +1,13 @@
+from app.state import MessageState
+
+# Função que define o fluxo do LangGraph com base na classificação da mensagem
+def route_message(state: MessageState) -> str:
+    match state["classification"]:
+        case "Fraude":
+            return "fraude"
+        case "Assédio":
+            return "assedio"
+        case "Reclamação":
+            return "reclamacao"
+        case _:
+            return "outro"
